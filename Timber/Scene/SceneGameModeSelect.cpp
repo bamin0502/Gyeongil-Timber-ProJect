8523335cc2a55f773be64c906279527b908f2285
방민호 <100828741+bamin0502@users.noreply.github.com>
2024-02-16 23:08:54 +0900

    도끼 이미지랑 배틀 관련 이미지 추가 나중에 백그라운드 이미지도 추가 예정

--- Timber/Scene/SceneGameModeSelect.cpp
+++ Timber/Scene/SceneGameModeSelect.cpp
@@ -1,2 +1,38 @@
 #include "pch.h"
 #include "SceneGameModeSelect.h"
+
+SceneGameModeSelect::SceneGameModeSelect(SceneIds id):Scene(id)
+{
+    
+}
+
+SceneGameModeSelect::~SceneGameModeSelect()
+{
+    
+}
+
+void SceneGameModeSelect::Init()
+{
+}
+
+void SceneGameModeSelect::Release()
+{
+    Scene::Release();
+}
+
+void SceneGameModeSelect::Enter()
+{
+    Scene::Enter();
+}
+
+void SceneGameModeSelect::Exit()
+{
+}
+
+void SceneGameModeSelect::Update(float dt)
+{
+    Scene::Update(dt);
+    cout<<"SceneGameModeSelect::Update"<<endl;
+}
+
+
--- Timber/Scene/SceneGameModeSelect.h
+++ Timber/Scene/SceneGameModeSelect.h
@@ -2,6 +2,16 @@
 
 class SceneGameModeSelect : public Scene
 {
+public:
+    SceneGameModeSelect(SceneIds id);
+    virtual ~SceneGameModeSelect();
 
+    void Init() override;
+    void Release() override;
+
+    void Enter() override;
+    void Exit() override;
+
+    void Update(float dt) override;
 };
 
