7e2d6f525451c76b064115c3d4b6adc170c5b94d
김정현 <wjdgus04175@gmail.com>
2024-02-17 16:51:19 +0900

    Title씬 구현 및 폰트, 타이틀이미지 업로드

--- Timber/Scene/SceneTitle.cpp
+++ Timber/Scene/SceneTitle.cpp
@@ -1,5 +1,58 @@
 #include "pch.h"
 #include "SceneTitle.h"
+#include "SpriteGo.h"
+#include "TextGo.h"
 
-SceneTitle::SceneTitle(SceneIds id) : Scene(id)
-{}
+SceneTitle::SceneTitle(SceneIds id)
+	:Scene(id)
+{
+}
+
+SceneTitle::~SceneTitle()
+{
+}
+
+void SceneTitle::Init()
+{
+	texResMgr.Load("graphics/starttitle.png");
+	fontResMgr.Load("fonts/neodgm.ttf");
+
+	startTitle = new SpriteGo("StartTitle");
+	startTitle->SetTexture("graphics/starttitle.png");
+	AddGo(startTitle);
+
+	uiText = new TextGo("Start Message");
+	uiText->Set(fontResMgr.Get("fonts/neodgm.ttf"), "Press Space to Start!", 100, sf::Color::White);
+	uiText->SetPosition({ 1920.f / 2,1080.f - 300 });
+	uiText->SetOrigin(Origins::MC);
+	AddGo(uiText);
+}
+
+void SceneTitle::Release()
+{
+	Scene::Release();
+}
+
+void SceneTitle::Update(float dt)
+{
+	Scene::Update(dt);
+
+	/*if (InputMgr::GetKeyDown(sf::Keyboard::Space))
+	{
+		SCENE_MGR.ChangeScene(SceneIds::SCENE_PLAYER_2_MODE);
+	}*/
+}
+
+void SceneTitle::Enter()
+{
+	Scene::Enter();
+}
+
+void SceneTitle::Exit()
+{
+}
+
+void SceneTitle::Draw(sf::RenderWindow& window)
+{
+	Scene::Draw(window);
+}
